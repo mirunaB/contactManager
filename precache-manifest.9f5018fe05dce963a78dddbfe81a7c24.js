bb3b9232851b34f6bd03898bc0db75a60480318d
Miruna <bdmiruna@gmail.com>
2019-07-05 13:02:21 +0300

    Updates

---
+++ precache-manifest.9f5018fe05dce963a78dddbfe81a7c24.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "879c0cd94702027148b124d7bdcdcfdb",
+    "url": "/contactManager/index.html"
+  },
+  {
+    "revision": "e1d24b3c84628cc688e5",
+    "url": "/contactManager/static/css/2.80eb6780.chunk.css"
+  },
+  {
+    "revision": "bef5340db444cd70728d",
+    "url": "/contactManager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "e1d24b3c84628cc688e5",
+    "url": "/contactManager/static/js/2.05f6d92b.chunk.js"
+  },
+  {
+    "revision": "bef5340db444cd70728d",
+    "url": "/contactManager/static/js/main.b641f4bc.chunk.js"
+  },
+  {
+    "revision": "49225faa5712f33c0f72",
+    "url": "/contactManager/static/js/runtime~main.63e620a2.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/contactManager/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/contactManager/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/contactManager/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/contactManager/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/contactManager/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
